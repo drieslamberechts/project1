6b38129da506a3b4d3b0b0b03e0267aac9b32c5c
frostraver <drieslamberechts@gmail.Com>
2013-04-28 14:31:22 +0200

    Added ResourceManager and GameManager

---
+++ src/com/EminusStudios/theunnamed/GameManager.java
@@ -0,0 +1,45 @@
+// SINGLETON CLASS
+// ------------------------------------
+
+package com.EminusStudios.theunnamed;
+
+public class GameManager
+{
+	private static GameManager INSTANCE;
+	
+	// VARIABLES
+	
+	private int mCurrentScore = 0;
+	private static final int INITIAL_SCORE = 0;
+	
+	//The constructor does not do anything for this singleton
+	GameManager()
+	{
+	}
+	
+	public static GameManager GetInstance()
+	{
+		if(INSTANCE == null)
+		{
+			INSTANCE = new GameManager();
+		}
+		return INSTANCE;
+	}
+	
+	// ---------------------------------------------------------------
+	// FILL IN THE DATA THAT HAS TO BE REMEMBERED THROUGHOUT THE GAME (GETTERS AND SETTERS)
+	// ---------------------------------------------------------------
+	
+	// EXAMPLE
+	// Get the current score
+	public int getCurrentScore()
+	{
+		return this.mCurrentScore;
+	}
+	
+	// Resetting the game simply means we must revert back to initial values.
+	public void resetGame()
+	{
+		this.mCurrentScore = GameManager.INITIAL_SCORE;
+	}
+}
---
+++ src/com/EminusStudios/theunnamed/ResourceManager.java
@@ -0,0 +1,34 @@
+package com.EminusStudios.theunnamed;
+
+import org.andengine.audio.sound.Sound;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.texture.region.ITextureRegion;
+
+public class ResourceManager 
+{
+	private static ResourceManager INSTANCE;
+	
+	// VARIABLES
+	
+	/* The variables listed should be kept public, allowing us easy
+	access to them when creating new Sprites, Text objects and to play soundfiles */
+	
+	public ITextureRegion mGameBackgroundTextureRegion;
+	public ITextureRegion mMenuBackgroundTextureRegion;
+	public Sound mSound;
+	public Font mFont;
+	
+	//The constructor does not do anything for this singleton
+	ResourceManager()
+	{
+	}
+	
+	public static ResourceManager GetInstance()
+	{
+		if(INSTANCE == null)
+		{
+			INSTANCE = new ResourceManager();
+		}
+		return INSTANCE;
+	}
+}
